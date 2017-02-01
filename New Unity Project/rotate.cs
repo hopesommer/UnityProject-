a19c98f0cb5a952ad4cc00d505b9d133218f588d
Hope <hopeonasommerday@gmail.com>
2017-01-31 17:18:14 -0700

    unity project updates
    unity project updates

---
+++ New Unity Project/rotate.cs
@@ -0,0 +1,40 @@
+<<<<<<< HEAD
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+    public float speed;
+
+
+    //group other variables together to have produce a more customizable output
+
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+    public float speed;
+
+>>>>>>> master
+    // Use this for initialization
+    void Start()
+    {
+        speed = Random.Range(.5f, 2.0f);
+    }
+<<<<<<< HEAD
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
+	}
+}
+=======
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
+	}
+}
+>>>>>>> master
